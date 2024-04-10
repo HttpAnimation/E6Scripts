0b6952dc24d0355422c47dc24abed6f01d52d341
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-09 18:21:43 -0700

    uses base

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 name = E6S
 URL = https://github.com/HttpAnimation/E6Scripts.git
 cd ~/
-git clone -b main https://github.com/HttpAnimation/E6Scripts.git
+git clone -b main $"URL"
 cd ~/
 mv E6Scripts E6S
 cd E6S
